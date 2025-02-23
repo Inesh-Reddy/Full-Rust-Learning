f4bdeee788e4f68ce3963d9d44f894d2f42cd803
Inesh-Reddy <ineshreddy007@gmail.com>
2025-02-23 09:05:12 +0530

    Rust: Functions -> Sum

--- Projects/crate_04_functions/src/main.rs
+++ Projects/crate_04_functions/src/main.rs
@@ -1,3 +1,10 @@
+fn do_sum(a: i32, b: i32) -> i32 {
+    return a+b;
+}
 fn main() {
     println!("Hello, world!");
+    let a = 10;
+    let b = 20;
+    let c = do_sum(a, b);
+    println!("Sum of {} & {} : {}", a,b,c);
 }
