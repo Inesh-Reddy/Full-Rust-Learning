7d1f2d323b040e1dc6b0f7504303dad5ac625161
Inesh-Reddy <ineshreddy007@gmail.com>
2025-02-28 08:39:36 +0530

    Rust: modules testing

--- Projects/crate_05_memory_mgmt/src/main.rs
+++ Projects/crate_05_memory_mgmt/src/main.rs
@@ -4,9 +4,10 @@ use crate_05_memory_mgmt::mutability;
 use crate_05_memory_mgmt::ownership;
 
 fn main() {
+    println!("Hello, World of Rust...");
     stack_heap::explain_stack_heap();
     mutability::explain_mutability();
     ownership::explain_ownership();
     borrowing::explain_borrowing();
-    println!("Hello, world!");
+    
 }
