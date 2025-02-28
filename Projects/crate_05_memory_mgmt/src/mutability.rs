3c908f84991dd3caa8e7d51e3db7cb93d0b2704b
Inesh-Reddy <ineshreddy007@gmail.com>
2025-02-28 08:53:07 +0530

    Rust: modules testing

--- Projects/crate_05_memory_mgmt/src/mutability.rs
+++ Projects/crate_05_memory_mgmt/src/mutability.rs
@@ -1,3 +1,11 @@
 pub fn explain_mutability() {
     println!("🔍 Mutability Example in Rust");
+    let x  = String::from("Immutable data doesnot give any issues..");
+    // x.push_str("I cannot be changed");//gives  error
+    println!("{}", x);
+
+    let mut y = String::from("Hey I am mutable, you can change me...");
+    y.push_str("They are changing me....");
+    println!("{}",y);
+
 }
